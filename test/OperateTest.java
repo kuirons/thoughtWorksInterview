20ea55b9646f7cd2c185b96f887de0cca3b7d1c4
kuirons <wgy199601@gmail.com>
2017-09-11 22:37:29 +0800

    testCheckTheRequestAttributesEnoughwhenInputParamRightShouldReturnTrue

--- test/OperateTest.java
+++ test/OperateTest.java
@@ -41,6 +41,8 @@ public void testCheckTheRequestAttributesEnough() throws Exception {
     assertEquals("whenInputSpaceShouldReturnFalse",false,new Operate().checkTheRequestAttributesEnough(""));
     assertEquals("whenInputParamLengthLessThan4ShouldReturnFalse",false,new Operate().checkTheRequestAttributesEnough("asdfasdfasdf1212412312"));
     assertEquals("whenInputParamLengthMoreThan5ShouldReturnFalse",false,new Operate().checkTheRequestAttributesEnough("as d fa sd fas df1 2124 12312"));
+    assertEquals("whenInputParamRightShouldReturnTrue",true,new Operate().checkTheRequestAttributesEnough("11 12 as 12"));
+    assertEquals("whenInputParamRightShouldReturnTrue",true,new Operate().checkTheRequestAttributesEnough("11 12 as 12 tt"));
 }
 
 
