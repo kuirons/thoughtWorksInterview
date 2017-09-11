46d074d97e227d8dc2c4d32d011f9513682bd8a8
kuirons <wgy199601@gmail.com>
2017-09-11 22:28:41 +0800

    testCheckTheRequestAttributesEnoughInputSpace

---
+++ test/OperateTest.java
@@ -0,0 +1,56 @@
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+
+import static org.junit.Assert.assertEquals;
+
+/** 
+* Operate Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sep 11, 2017</pre> 
+* @version 1.0 
+*/ 
+public class OperateTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: checkTheRequestWhetherInvalid(String request) 
+* 
+*/ 
+@Test
+public void testCheckTheRequestWhetherInvalid() throws Exception { 
+//TODO: Test goes here...
+} 
+
+/** 
+* 
+* Method: checkTheRequestAttributesEnough(String request) 
+* 
+*/ 
+@Test
+public void testCheckTheRequestAttributesEnough() throws Exception {
+    assertEquals("whenInputSpaceShouldReturnFalse",false,new Operate().checkTheRequestAttributesEnough(""));
+}
+
+
+/**
+* 
+* Method: getRequestBean() 
+* 
+*/ 
+@Test
+public void testGetRequestBean() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
