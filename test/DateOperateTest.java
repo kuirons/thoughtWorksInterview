5a17f00f999d99661d4d107eb652061ea56fa1aa
kuirons <wgy199601@gmail.com>
2017-09-11 22:45:06 +0800

    testCheckTheDateWhetherInvalidWhenInputNullShouldReturnFalse

---
+++ test/DateOperateTest.java
@@ -0,0 +1,56 @@
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+
+import static junit.framework.TestCase.assertEquals;
+
+/** 
+* DateOperate Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sep 11, 2017</pre> 
+* @version 1.0 
+*/ 
+public class DateOperateTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: checkTheDateWhetherInvalid(String date) 
+* 
+*/ 
+@Test
+public void testCheckTheDateWhetherInvalid() throws Exception { 
+//TODO: Test goes here...
+    assertEquals("WhenInputNullShouldReturnFalse",false,new DateOperate().checkTheDateWhetherInvalid(""));
+} 
+
+/** 
+* 
+* Method: checkTheTimeWhetherInvalid(String request) 
+* 
+*/ 
+@Test
+public void testCheckTheTimeWhetherInvalid() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: checkTheSiteWhetherInvalid(String request) 
+* 
+*/ 
+@Test
+public void testCheckTheSiteWhetherInvalid() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
